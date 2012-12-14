2ef119ede4b0ba6e0a2b4fdca98d69c24010a37a
Jay Tuckey <jay.tuckey@gmail.com>
2012-12-14 16:49:19 +0930

    Added some comments to my midterm assignments.

--- midterm-1.cpp
+++ midterm-1.cpp
@@ -7,14 +7,21 @@ using namespace std;
 #define NUMC 10
 
 int main() {
+	// Create a string to filter input, and an array of letters.
 	string input;
 	char letters[NUMC], c = 127;
 	
+	// Function prototype
 	char firstLetter(char, char);
 	
+	// Request the user to enter ten letters
 	cout << "Enter 10 letters" << endl;
 	for(int i = 0; i< NUMC; i++) {
 		cout << "Please enter a letter: ";
+		// Input the letters into a string then take the first letter
+		// from the string and add it to the array. This prevents
+		// newlines, and multiple entered characters from skipping
+		// input opportunities
 		cin >> input;
 		letters[i] = input[0];
 	}
--- midterm-2.cpp
+++ midterm-2.cpp
@@ -4,12 +4,19 @@
 using namespace std;
 
 int main() {
+	// Create a variable to store the temperature
 	int temp;
 	
+	// Function prototypes
 	void getTemp(int *);
 	void printActivity(int);
 	
+	// Request the user to enter the temperature. Note that the address
+	// of the variable is passed, so there in no need for the function
+	// to return a value; the value is passed back using a pointer
 	getTemp(&temp);
+	
+	// Print the activity to do
 	printActivity(temp);
 }
 
@@ -21,6 +28,7 @@ void getTemp(int *temp) {
 void printActivity(int temp) {
 	cout << "Activity for today is ";
 
+	// Choose an activity based on the temperature
 	if(temp > 85)
 		cout << "swimming";
 	else if(temp > 70)
