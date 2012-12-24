3675d5ec09ba027bd0b0c3232012beec3a57326e
Jay Tuckey <jay.tuckey@gmail.com>
2012-12-24 14:43:21 +0930

    working on assignment 4

--- assignment4-B.cpp
+++ assignment4-B.cpp
@@ -3,6 +3,39 @@
 #include <iostream>
 using namespace std;
 
+class personType{
+public:
+	personType() {
+		firstName = "NotSet";
+		lastName = "NotSet";
+	}
+	
+	personType(string first, string last) {
+		firstName = first;
+		lastName = last;
+	}
+	
+	void setFirstName(string name) {
+		firstName = name;
+	}
+	void setLastName(string name) {
+		lastName = name;
+	}
+	
+	void printName() {
+		cout << firstName << " " << lastName << endl;
+	}
+	
+private:
+	string firstName;
+	string lastName;
+	
+};
+
 int main() {
-  
+	personType bob;
+	personType richard("Richard", "Dominique");
+	bob.printName();
+	richard.printName();
+	
 }
