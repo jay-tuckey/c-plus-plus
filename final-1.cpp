fcf015e45be8448870d705fb95e6c89d929d34d6
Jay Tuckey <jay.tuckey@gmail.com>
2013-01-20 19:42:16 +0930

    Working on final assignments.

--- final-1.cpp
+++ final-1.cpp
@@ -9,6 +9,7 @@ class Animal {
 public:
   Animal(int p = 1) {
     position = p;
+    won = false;
   }
   int getPosition() {
     return position;
@@ -18,45 +19,75 @@ public:
     // Implement in each animal
   }
   
+  void tryMove(int d) {
+    if(position + d > 1)
+      position += d;
+    else
+      position = 1;
+
+  }
+
   int getRand() {
     return rand () % 10;
   }
+
+  void checkWin() {
+    if(position >= 70)
+      won = true;
+  }
+
+  // Variable to indicate if they have won the race
+  bool won; 
 private:
   
   int position;
-};  
+};  // End of class Animal
 
 class Rabbit : public Animal {
 public:
   virtual void move() {
     int n = getRand();
-    if(n <= 4) {
-      cout << "fast plod" << endl;
-    } else if( n <= 6) {
-      cout << "slip" << endl;
+    cout << "The rabbit ";
+    if(n <= 1) {
+      cout << "falls asleep.";
+    } else if( n <= 3) {
+      tryMove(9);
+      cout << "has a big hop.";
+    } else if( n <= 4) {
+      tryMove(-12);
+      cout << "has a big slip.";
+    } else if( n <= 7) {
+      tryMove(1);
+      cout << "has a small hop.";
     } else if( n <= 9) {
-      cout << "slow plod" << endl;
+      tryMove(-2);
+      cout << "has a small slip.";
     }
+
+    cout << " It is at square " << getPosition() << endl;
+    checkWin();
   }
-};
+}; // End of class Rabbit
 
 class Tortoise : public Animal {
 public:
   virtual void move() {
     int n = getRand();
-    if(n <= 1) {
-      cout << "sleep" << endl;
-    } else if( n <= 3) {
-      cout << "big hop" << endl;
-    } else if( n <= 4) {
-      cout << "big slip" << endl;
-    } else if( n <= 7) {
-      cout << "small hop" << endl;
+    cout << "The tortoise makes a ";
+    if(n <= 4) {
+      tryMove(3);
+      cout << "fast plod.";
+    } else if( n <= 6) {
+      tryMove(-6);
+      cout << "slip.";
     } else if( n <= 9) {
-      cout << "small slip" << endl;
+      tryMove(1);
+      cout << "slow plod.";
     }
+    cout << " It is at square " << getPosition() << endl;
+    checkWin();
   }
-};
+}; // End of class Tortoise
 
 
 int main() {
@@ -66,7 +97,20 @@ int main() {
   Animal *t = &tortoise, *r = &rabbit;
 
   cout << "Bang! Off they go!" << endl;
-  t->move();
-  r->move();
+  while(!r->won && !t->won) {
+    t->move();
+    r->move();
+    if(t->getPosition() == r->getPosition())
+      cout << "BUMP!" << endl;
+    cout << "Press Enter to continue";
+    cin.get();
+  }
+  cout << endl;
+  if(r->won == t->won)  // They have drawn the race
+    cout << "Tie score -- no winner!" << endl;
+  else if(r->won)
+    cout << "Yay! The rabbit won! He hops the fastest!" << endl;
+  else
+    cout << "Woo-hooo! Slow and steady wins the race! Congratulations, tortoise!" << endl;
 
 }
---
+++ final-2.cpp
@@ -0,0 +1,208 @@
+/* Here we go */
+
+#include <iostream>
+#include <stdexcept>
+using namespace std;
+
+
+class AlarmClock {
+public:
+  // Only included one constructor, since it does both the jobs that were required in the question
+  AlarmClock(int h = 12, int m = 0, int s = 0, bool am = true) {
+    setAm(am);
+    setHours(h);
+    setMins(m);
+    setSecs(s);
+    
+  }
+
+  void operator++(int s) {
+    if(secs != 59)
+      ++secs;
+    else {
+      secs = 0;
+      if(mins != 59)
+	++mins;
+      else {
+	mins = 0;
+	setHours(hours + 1);
+      }
+    }
+
+    checkAlarm();
+  }
+
+  void checkAlarm() {
+    if(!aon)
+      return;
+    if(hours == ahours && mins == amins && secs == asecs)
+      cout << "WAKE UP!!!" << endl;
+  }
+
+  void setHours(int h) {
+    if(h < 0)
+      throw invalid_argument("Can't set the hours to less than 0");
+    if(0 == h) {
+      hours = 12;
+      alternateAm();
+    } else if(h < 12) {
+      hours = h; 
+    } else if(12 == h) {
+      hours = h;
+      alternateAm();
+    } else if(h < 24) {
+      hours = h-12;
+      alternateAm();
+    } else
+      throw invalid_argument("Can't set the hour to greater than 24");
+    
+  }
+  
+  int getHours() {
+    return hours;
+  }
+  
+  void setMins(int m) {
+    if(m < 0)
+      throw invalid_argument("Can't set the minute to less than 0");
+    else if(m < 60) {
+      mins = m;
+    } else
+      throw invalid_argument("Can't set the minute to greater than 59");
+  }
+
+  int getMins() {
+    return mins;
+  }
+  
+  void setSecs(int s) {
+    if(s < 0)
+      throw invalid_argument("Can't set the second to less than 0");
+    else if(s < 60) {
+      secs = s;
+    } else
+      throw invalid_argument("Can't set the second to greater than 59");
+  }
+
+  int getSecs() {
+    return secs;
+  }
+
+  void setAm(bool am) {
+    isam = am;
+  }
+
+  bool getAm() {
+    return isam;
+  }
+  
+  void alternateAm() {
+    if(isam)
+      isam = false;
+    else
+      isam = true;
+  }
+
+  void setAlarm(int h, int m, int s, bool am) {
+    asetAm(am);
+    asetHours(h);
+    asetMins(m);
+    asetSecs(s);
+    aon = true;
+  }
+
+  void asetHours(int h) {
+    if(h < 0)
+      throw invalid_argument("Can't set the hours to less than 0");
+    if(0 == h) {
+      ahours = 12;
+      aalternateAm();
+    } else if(h < 13) {
+      ahours = h;
+    } else if(h < 25) {
+      ahours = h-12;
+      aalternateAm();
+    } else
+      throw invalid_argument("Can't set the hour to greater than 24");
+  }
+
+  void aalternateAm() {
+    if(aisam)
+      aisam = false;
+    else
+      aisam = true;
+  }
+
+  int agetHours() {
+    return ahours;
+  }
+  
+  void asetMins(int m) {
+    if(m < 0)
+      throw invalid_argument("Can't set the minute to less than 0");
+    else if(m < 60) {
+      amins = m;
+    } else
+      throw invalid_argument("Can't set the minute to greater than 59");
+  }
+
+  int agetMins() {
+    return amins;
+  }
+  
+  void asetSecs(int s) {
+    if(s < 0)
+      throw invalid_argument("Can't set the second to less than 0");
+    else if(s < 60) {
+      asecs = s;
+    } else
+      throw invalid_argument("Can't set the second to greater than 59");
+  }
+
+  int agetSecs() {
+    return asecs;
+  }
+
+  void asetAm(bool am) {
+    aisam = am;
+  }
+
+  bool agetAm() {
+    return aisam;
+  }
+
+  void printTime() {
+    cout << "Hours:" << hours 
+	 << " Mins:" << mins
+	 << " Secs:" << secs
+	 << " AM:" << isam << endl;
+  }
+private:
+  int hours; // Current time - hours
+  int mins; // Current time - minutes
+  int secs; // Current time - seconds
+  bool isam; // Current time - AM/PM
+  bool aon; // Alarm - On/Off
+  int ahours; // Alarm - hours
+  int amins; // Alarm - minutes
+  int asecs; // Alarm - seconds
+  bool aisam; // Alarm - AM/PM
+}; // End class AlarmClock
+
+int main() {
+  AlarmClock a(11, 59, 57, true), b(4, 15, 50, true), c(15) ;
+  int counter;
+
+  a.setAlarm(12,1,30,false);
+  cout << "Time is: ";
+  while(counter != 0) {
+    a.printTime();
+    cout << "Enter 0 to quit. How much do you want to increment the time by, in seconds? ";
+    cin >> counter;
+    
+    for(int i = 0; i < counter; i++) {
+      a++;
+    }
+  }
+  
+}
