82ea6e3d8da697f8d0c06bc42368f23fd85fe425
Jay Tuckey <jay.tuckey@gmail.com>
2013-04-02 21:04:58 +0930

    Working on assignment 4.

---
+++ data-structures/chapter7/Linked/sortedType.h
@@ -0,0 +1,27 @@
+#include "ItemType.h"
+// Header file for Sorted List ADT.  
+struct NodeType;
+
+class SortedType
+{
+public:
+  SortedType();     // Class constructor	
+  ~SortedType();    // Class destructor
+
+  bool IsFull() const;
+  int  GetLength() const;
+  void MakeEmpty();
+  ItemType GetItem(ItemType& item, bool& found);
+  void PutItem(ItemType item); 
+  void DeleteItem(ItemType item);
+  void ResetList();
+  ItemType GetNextItem();
+  void Search(ItemType value, bool& found);
+
+private:
+  NodeType* listData;
+  int length;
+  NodeType* currentPos;
+  void RecursiveSearch(ItemType, bool&, NodeType*);
+};
+
